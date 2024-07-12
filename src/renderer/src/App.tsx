3eadbaa564d3e7b54f05a27cd05f47f38e851910
Remy-Tresor250 <niyitegekatresor@gmail.com>
2024-07-12 12:27:34 +0200

    initial commit

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,11 @@
+import VideoCall from "./pages/VideoCall";
+
+const App = (): JSX.Element => {
+  return (
+    <main className={`w-[100%] h-[100%]`}>
+      <VideoCall />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/renderer/src/pages/VideoCall.tsx
@@ -0,0 +1,5 @@
+const VideoCall = (): JSX.Element => {
+  return <div>VideoCall</div>;
+};
+
+export default VideoCall;
